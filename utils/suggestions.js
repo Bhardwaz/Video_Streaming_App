fed99dda942a5c9adc54cb9a7b76e8fa19086237
Sumit Bhardwaj <sumitbhardwaj106@gmail.com>
2023-05-22 23:19:40 +0530

    add searchBar d1

---
+++ utils/suggestions.js
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const suggestions = createSlice({
+    name : 'suggestions',
+    initialState:{
+        results:[]
+    },
+    reducers:{
+        queries:() => {
+         state.results.add
+        }
+    }
+})
