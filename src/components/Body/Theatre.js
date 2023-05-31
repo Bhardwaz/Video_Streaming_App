8a6bc46adb20daad3d69af9cfcf4a6cff756b629
Sumit Bhardwaj <sumitbhardwaj106@gmail.com>
2023-05-31 10:37:40 +0530

    rec compo add

--- src/components/Body/Theatre.js
+++ src/components/Body/Theatre.js
@@ -1,29 +1,21 @@
 import React from 'react'
 import Sidebar from './Sidebar'
 import { useSearchParams } from 'react-router-dom'
+import Recommendation from './Recommendation'
 
 const Watch = () => {
   const [searchParams] = useSearchParams()
   const id = searchParams.get('v')
   return(
-    <div className='w-[80%] h-[40%] mx-auto'>
-      <iframe className='grid grid-cols-1 w-[100%] h-full' src={"https://www.youtube.com/embed/" + id } title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen="yes"></iframe>
-    </div>
-  )
-}
-
-const Recommendation = () => {
-  
-  return(
-    <div className='text-white'>
-    Recommendation
+    <div className='w-[80%] h-[80%] mx-auto'>
+      <iframe className='grid grid-cols-1 w-[100%] h-[100%]' src={"https://www.youtube.com/embed/" + id } title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen="yes"></iframe>
     </div>
   )
 }
 
 const Theatre = () => {
   return (
-    <div className='text-gray-200 text-center flex flex-col mx-auto w-[100%] h-[100vh] relative gap-10'>
+    <div className='text-gray-200 text-center flex flex-col mx-auto w-[100%] h-[150vh] relative gap-10'>
       <Sidebar />
       <Watch />
       <Recommendation />
