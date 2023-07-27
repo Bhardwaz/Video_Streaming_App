95ae5aacbce40049f8a60e536cacd901c91a97b0
Sumit Bhardwaj <sumitbhardwaj106@gmail.com>
2023-07-27 18:29:04 +0530

    cors fix

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import ReactDOM  from "react-dom/client";
+import ReactDOM from "react-dom/client";
 import React from "react";
 import Header from "./src/components/Layout/Header";
 import { Provider } from "react-redux";
@@ -8,34 +8,36 @@ import { RouterProvider, createBrowserRouter, Outlet } from "react-router-dom";
 import Theatre from "./src/components/Body/Theatre";
 import SearchResults from "./src/components/Body/SearchResults";
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 const AppLayout = () => {
-    return(
-        <Provider store={store}>
-           <Header/>
-           <Outlet /> 
-        </Provider>
-    )
-}
+  return (
+    <Provider store={store}>
+      <Header />
+      <Outlet />
+    </Provider>
+  );
+};
 
-const appRouter = createBrowserRouter([{
-    path:'/',
-    element:<AppLayout />,
-    children:[
-        {
-            path:'/',
-            element:<VideoContainer />
-        },
-        {
-            path:'/theatre',
-            element:<Theatre />
-        },
-        {
-            path:'/results',
-            element:<SearchResults />
-        }
-    ]
-}])
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <AppLayout />,
+    children: [
+      {
+        path: "/",
+        element: <VideoContainer />,
+      },
+      {
+        path: "/theatre",
+        element: <Theatre />,
+      },
+      {
+        path: "/results",
+        element: <SearchResults />,
+      },
+    ],
+  },
+]);
 
-root.render(<RouterProvider router={appRouter} />)
+root.render(<RouterProvider router={appRouter} />);
